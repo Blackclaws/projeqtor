d35461b635e4c977caf39bec1cb4feb548e818e0
projeqtor <projeqtor@f9421885-1815-481d-89b5-6d9585689fe2>
2016-08-14 23:45:13 +0000

    V5.5

--- projeqtor/files/config/parameters.php
+++ projeqtor/files/config/parameters.php
@@ -1,7 +1,7 @@
 <?php
 // =======================================================================================
 // Automatically generated parameter file
-// on 2016-08-13 17:00:11
+// on 2016-08-14 15:03:03
 // =======================================================================================
 $paramDbType='mysql'; $paramDbPort='3306'; $paramDbUser='root'; $paramDbPassword='mysql';
 $paramDbName='projeqtor_v55';$paramDbPrefix='';
@@ -10,7 +10,7 @@ $logFile='../files/logs/projeqtor_${date}.log';
 $logLevel='3';
 $flashReport=true;
 $debugQuery=false;
-$debugJsonQuery=false;
+$debugJsonQuery=true;
 $debugPerf=true;
 $debugReport=true;
 $i18nNocache=true;
