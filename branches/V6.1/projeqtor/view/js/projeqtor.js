057941d1cb314bce41497cd9efe5283f2a85361c
projeqtor <projeqtor@f9421885-1815-481d-89b5-6d9585689fe2>
2017-02-05 00:45:31 +0000

    V6.1

--- branches/V6.1/projeqtor/view/js/projeqtor.js
+++ branches/V6.1/projeqtor/view/js/projeqtor.js
@@ -694,7 +694,6 @@ function loadContent(page, destination, formName, isResultMessage,
           if (destination == "detailDiv" && dojo.byId('objectClass')
               && dojo.byId('objectClass').value && dojo.byId('objectId')
               && dojo.byId('objectId').value) {
-            console.log("sur select element");
             stockHistory(dojo.byId('objectClass').value,
                 dojo.byId('objectId').value);
           } 
@@ -3674,21 +3673,24 @@ function ckEditorReplaceAll() {
 }
 var maxEditorHeight = Math.round(screen.height * 0.6);
 
-
-
+var currentEditorIsNote=false;
 function ckEditorReplaceEditor(editorName, numEditor) {
   var height = 200;
   if (dojo.byId("ckeditorHeight"+numEditor)) {
     height=dojo.byId("ckeditorHeight"+numEditor).value;
   }
-  if (editorName == 'noteNote')
+  currentEditorIsNote=false;
+  if (editorName == 'noteNote') {
     height = maxEditorHeight - 150;
+    currentEditorIsNote=true;
+  }
   var readOnly = false;
   if (dojo.byId('ckeditor' + numEditor + 'ReadOnly')
       && dojo.byId('ckeditor' + numEditor + 'ReadOnly').value == 'true') {
     readOnly = true;
   }
   autofocus = (editorName == 'noteNote') ? true : false;
+  
   editorArray[numEditor] = CKEDITOR.replace(editorName, {
     customConfig : 'projeqtorConfig.js',
     filebrowserUploadUrl : '../tool/uploadImage.php',
