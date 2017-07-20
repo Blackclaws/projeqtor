87de9bfc81a9f7e35de0f371dc87b7ec012f04f2
projeqtor <projeqtor@f9421885-1815-481d-89b5-6d9585689fe2>
2017-07-20 23:45:17 +0000

    V6.3

--- branches/V6.3/projeqtor/view/js/projeqtor.js
+++ branches/V6.3/projeqtor/view/js/projeqtor.js
@@ -2247,6 +2247,9 @@ function setSelectedProject(idProject, nameProject, selectionField) {
         } else if (dojo.byId('objectClassManual')
             && dojo.byId('objectClassManual').value == 'Today') {
           loadContent("../view/today.php", "centerDiv");
+        } else if (dojo.byId('objectClassManual')
+            && dojo.byId('objectClassManual').value == 'Plugin_kanban') {
+          loadContent("../plugin/kanban/kanbanViewMain.php", "centerDiv");        
         } else if (dojo.byId('objectClassManual') && dojo.byId('objectClassManual').value == 'ActivityStream') {
           loadContent("../view/activityStreamList.php", "activityStreamListDiv", "activityStreamForm");      
         } else if (dojo.byId('objectClassManual')
@@ -4382,4 +4385,32 @@ function switchNoteStatus(idNote) {
     }
  });
   
+}
+function switchNotesPrivacyStream() {
+  if (! dojo.byId("notePrivacyStream") || !dojo.byId("notePrivacyStreamUserTeam") || !dojo.byId("notePrivacyStreamDiv") ) {
+    return;
+  }
+  var privacy=dojo.byId("notePrivacyStream").value;
+  var team=dojo.byId("notePrivacyStreamUserTeam").value;
+  if (privacy=="2") {
+    dojo.byId("notePrivacyStream").value="3";
+    dojo.byId("notePrivacyStreamDiv").className="iconFixed16";
+    dojo.byId("notePrivacyStreamDiv").title=i18n("colIdPrivacy")+" : "+i18n("private");
+  } else if (privacy=="3") {
+    dojo.byId("notePrivacyStream").value="1";
+    dojo.byId("notePrivacyStreamDiv").className="";
+    dojo.byId("notePrivacyStreamDiv").title=i18n("colIdPrivacy")+" : "+i18n("public");
+  } else {
+    if (team) {
+      dojo.byId("notePrivacyStream").value="2";
+      dojo.byId("notePrivacyStreamDiv").className="iconTeam16";
+      dojo.byId("notePrivacyStreamDiv").title=i18n("colIdPrivacy")+" : "+i18n("team");
+    } else {
+      dojo.byId("notePrivacyStream").value="3";
+      dojo.byId("notePrivacyStreamDiv").className="iconFixed16";
+      dojo.byId("notePrivacyStreamDiv").title=i18n("colIdPrivacy")+" : "+i18n("private");
+    }
+  }
+  var currentClass=(dojo.byId('objectClass'))?dojo.byId('objectClass').value:'';
+  saveDataToSession("privacyNotes"+currentClass,dojo.byId("notePrivacyStream").value,true);
 }
