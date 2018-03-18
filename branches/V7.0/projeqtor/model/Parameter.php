6765b63c2225ea48076386767a333ef0c68197b5
projeqtor <projeqtor@f9421885-1815-481d-89b5-6d9585689fe2>
2018-03-18 00:46:23 +0000

    V7.0

--- branches/V7.0/projeqtor/model/Parameter.php
+++ branches/V7.0/projeqtor/model/Parameter.php
@@ -280,9 +280,11 @@ class Parameter extends SqlElement {
       case 'productVersionOnDelivery' :
       case 'manageAccountable':
       case 'manageComponentOnRequirement' :   //ADD qCazelles - Add Component to Requirement - Ticket 171
+      case 'sortVersionComboboxNameDesc' : //ADD qCazelles - Sort versions in combobox - Ticket 89
       case 'manageMilestoneOnItems' :
       case 'autoLinkMilestone' :
       case 'milestoneFromVersion' :
+      case 'dontAddClosedDeliveredVersionToProject' : //ADD qCazelles - Dont add closed and delivered versions to Project - Ticket 181 
       case 'mailGroupActive' :
         $list=array('NO'=>i18n('displayNo'),
                     'YES'=>i18n('displayYes')); 
@@ -724,7 +726,9 @@ class Parameter extends SqlElement {
                              	'versionCompatibility'=>'list',
                               //'sectionDelivery'=>'section', //ADD qCazelles
                               'productVersionOnDelivery'=>'list',
+                              'sortVersionComboboxNameDesc'=>'list', //ADD qCazelles - Sort versions in combobox - Ticket 89
                               'manageComponentOnRequirement'=>'list',   //ADD qCazelles - Add Component to Requirement - Ticket 171
+                              'dontAddClosedDeliveredVersionToProject'=>'list', //ADD qCazelles - Dont add closed and delivered versions to Project - Ticket 181
                         'tabMail'=>"tab",   
                           'newColumnMailLeft'=>'newColumn',
                             'sectionMail'=>'section',
