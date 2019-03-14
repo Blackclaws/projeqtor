ac84f54cdabf89f3d8ecc457272f5d92d160593f
projeqtor <projeqtor@f9421885-1815-481d-89b5-6d9585689fe2>
2019-03-14 00:46:53 +0000

    V8.0

--- branches/V8.0/projeqtor/model/UserMain.php
+++ branches/V8.0/projeqtor/model/UserMain.php
@@ -1601,5 +1601,59 @@ debugTraceLog("User->authenticate('$paramlogin', '$parampassword')" );
     }
     return $result;
   } 
+  
+  //gautier #itemTypeRestriction
+  public function getItemTypeRestriction($obj,$objectClass,$user,$showIdle,$showIdleProjects) {
+    $table=$obj->getDatabaseTableName();
+    $resultOr = '';
+    $resultAnd = '';
+    $objType = $obj->getDatabaseColumnName($objectClass . 'Type');
+    $restrictType = new RestrictType();
+    $listRestrictType = $restrictType->getSqlElementsFromCriteria(array('className'=>$objType,'idProfile'=>$user->idProfile));
+    foreach ($listRestrictType as $typeRestrict){
+      $tabListIdRestrictType[] = $typeRestrict->idType;
+    }
+    if(isset($tabListIdRestrictType)){
+      $inTabListIdRestrictType = transformValueListIntoInClause($tabListIdRestrictType);
+      $resultAnd.= "  and ($table.id$objType in $inTabListIdRestrictType " ;
+    }else{
+      $resultAnd.= "  and ( 1=1" ;
+    }
+    if (getSessionValue('project')=='*'){
+      $resultAnd.= " and ($table.idProject in " . getVisibleProjectsList(! $showIdleProjects). " or $table.idProject is null)";
+    }
+      $monTab = getVisibleProjectsList(! $showIdleProjects);
+      $monTab = substr($monTab, 1);
+      $monTab = substr($monTab,0,-1);
+      $tabVisibleProjet = explode(', ', $monTab);
+      
+    $listProjOtherProfile = $user->getSpecificAffectedProfiles($showIdle);
+    foreach ($listProjOtherProfile as $id=>$idProfile){
+      if($idProfile != $user->idProfile){
+        $tabProfileByProj[$id]=$idProfile;
+      }
+    }
+    if(isset($tabProfileByProj)){
+      foreach ($tabProfileByProj as $idProj=>$idProfile){
+        if(in_array($idProj,$tabVisibleProjet)){
+          $listRestrictType = $restrictType->getSqlElementsFromCriteria(array('className'=>$objType,'idProfile'=>$idProfile));
+          $tabListIdRestrictTypeByProject = array();
+          foreach ($listRestrictType as $typeRestrict){
+            $tabListIdRestrictTypeByProject[] = $typeRestrict->idType;
+          }
+          if($tabListIdRestrictTypeByProject){
+            $tabListIdRestrictTypeByProject = transformValueListIntoInClause($tabListIdRestrictTypeByProject);
+            $resultOr.= " or ($table.idProject=$idProj and ( $table.id$objType in $tabListIdRestrictTypeByProject)) " ;
+          }else{
+            $resultOr.= " or ($table.idProject=$idProj ) " ;
+          }
+        }
+      }
+      $listIdProj = transformListIntoInClause($tabProfileByProj);
+      $resultAnd .= " and  $table.idProject not in $listIdProj ) ";
+    }
+    $result = $resultAnd.$resultOr;
+    return $result;
+  }
 }
 ?>
